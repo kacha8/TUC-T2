830e95b178263efdbcca08b1beaebaacd1f4f31c
kacha8 <kacha8@student.monash.edu>
2016-05-17 23:34:57 +1000

    removal of the init_db call
    since after reading the tutorial it turns out this is not call in the
    script

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -3,7 +3,6 @@ import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 from contextlib import closing
-from flaskr import init_db
 
 
 # configuration
@@ -19,21 +18,18 @@ app.config.from_object(__name__)
 
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
-init_db()
+if __name__ == '__main__':
+	app.debug = True
+	app.run()
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
-
-if __name__ == '__main__':
-	app.run()
 
 def init_db():
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
-
-
 
 @app.before_request
 def before_request():
