ce0c3a13967d88d180d1214060f6e4fc8d6e9e2d
kacha8 <kacha8@student.monash.edu>
2016-05-18 01:49:53 +1000

    removing initdb
    im tired of this shit

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -32,14 +32,6 @@ def init_db():
         db.cursor().executescript(f.read())
     db.commit()
 
-
-@app.cli.command('initdb')
-def initdb_command():
-    """Creates the database tables."""
-    init_db()
-    print('Initialized the database.')
-
-
 def get_db():
     """Opens a new database connection if there is none yet for the
     current application context.
