33d8ba3f22b59e649199ef5612e7208ec22a6149
kacha8 <kacha8@student.monash.edu>
2016-05-19 23:56:39 +1000

    i duunoooo

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -26,21 +26,13 @@ def connect_db():
 
 
 def init_db():
-<<<<<<< HEAD
-    """Initializes the database."""
- with app.app_context():
-     db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-=======
-	"""Initializes the database."""
-	with app.app_context():
-		db = get_db()
-		with app.open_resource('schema.sql', mode='r') as f:
-			db.cursor().executescript(f.read())
-		db.commit()
->>>>>>> origin/master
+		"""Initializes the database."""
+		with app.app_context():
+			db = get_db()
+			with app.open_resource('schema.sql', mode='r') as f:
+				db.cursor().executescript(f.read())
+			db.commit()
+
 
 def get_db():
     """Opens a new database connection if there is none yet for the
